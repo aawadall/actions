cefbbec896c0c999842eeb2310bd1dd6d565f3d9
Ahmed Awadallah <aawadall@ualberta.ca>
2020-09-20 12:19:33 -0400

    use receive predicate

--- actor/Actors/BetaActor.cs
+++ actor/Actors/BetaActor.cs
@@ -12,7 +12,13 @@ namespace actor.Actors
         {
 
             System.Console.WriteLine($"Creating a {this.GetType().Name}");
-            Receive<SampleMessage>(message => HandleSampleMessage(message));
+            Receive<SampleMessage>(message => HandleSampleMessage(message), message => message.Id == 0x10);
+            Receive<SampleMessage>(DefaultMessageHandler);
+        }
+
+        private void DefaultMessageHandler(SampleMessage obj)
+        {
+            System.Console.WriteLine($"Default message handler");
         }
 
         private void HandleSampleMessage(SampleMessage message) 
